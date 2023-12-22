3fd5ed319c569e4ca4342df2ea93da14b3f9d94b
Hart102 <hartongit102@gmail.com>
2023-12-22 12:12:15 -0500

    created a rating endpoint

--- routes/propertyRoute/index.js
+++ routes/propertyRoute/index.js
@@ -8,6 +8,7 @@ const { search } = require("../../controllers/property/search");
 const { create_property } = require("../../controllers/property/create");
 const { update_property } = require("../../controllers/property/update");
 const { delete_property } = require("../../controllers/property/delete");
+const { rate_property } = require("../../controllers/property/rating");
 
 router.get("/", get_all_properties);
 router.get("/single/:id", get_single_property);
@@ -15,6 +16,7 @@ router.post("/search/:query", search);
 router.post("/create", create_property);
 router.put("/update", update_property);
 router.delete("/delete", delete_property);
+router.post("/rate", rate_property);
 
 
 module.exports = router;
